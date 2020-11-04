464d763e4c71569bad87e24ac100bf39b8692972
PabloVillapunMartin <pvillapu@ucm.es>
2020-11-04 11:16:04 +0100

    cambio inclinacion luz direccional

--- IG2App/IG2App.cpp
+++ IG2App/IG2App.cpp
@@ -125,7 +125,7 @@ void IG2App::setupScene(void)
   //mLightNode = mCamNode->createChildSceneNode("nLuz");
   mLightNode->attachObject(luz);
 
-  mLightNode->setDirection(Ogre::Vector3(0, 0, -1));  //vec3.normalise();
+  mLightNode->setDirection(Ogre::Vector3(0, -0.75, -1));  //vec3.normalise();
   //mLightNode->setDirection(Ogre::Vector3(0, 1, 0));  //vec3.normalise();
   //mLightNode->setDirection(Ogre::Vector3(0, -1, 0));  //vec3.normalise();
   //lightNode->setPosition(0, 0, 1000);
