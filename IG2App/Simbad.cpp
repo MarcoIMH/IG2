bf457aec24a6fb977b8099a279f88b828434fbbf
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2020-11-11 12:06:13 +0100

    P1.5 Ejercicio 33

--- IG2App/Simbad.cpp
+++ IG2App/Simbad.cpp
@@ -6,9 +6,18 @@ Simbad::Simbad(Ogre::SceneNode* mNode) :EntidadIG(mNode)
 {
 	sinbad = mSM->createEntity("Sinbad.mesh");
 	mNode->attachObject(sinbad);
+
 	as_Dance = sinbad->getAnimationState("Dance");
-	as_Dance->setEnabled(true);
-	as_Dance->setLoop(true);
+	as_Dance->setEnabled(false);
+	as_Dance->setLoop(false);
+
+	as_RunBase = sinbad->getAnimationState("RunBase");
+	as_RunBase->setEnabled(false);
+	as_RunBase->setLoop(false);
+
+	as_RunTop = sinbad->getAnimationState("RunTop");
+	as_RunTop->setEnabled(true);
+	as_RunTop->setLoop(true);
 
 	appListeners.push_back(this);
 }
@@ -21,4 +30,6 @@ Simbad::~Simbad()
 void Simbad::frameRendered(const Ogre::FrameEvent& evt)
 {
 	if(as_Dance->getEnabled()) as_Dance->addTime(evt.timeSinceLastEvent);
+	else if (as_RunBase->getEnabled()) as_RunBase->addTime(evt.timeSinceLastEvent);
+	else if (as_RunTop->getEnabled()) as_RunTop->addTime(evt.timeSinceLastEvent);
 }
