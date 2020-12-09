fa49259830ae7fc55e3260189b6213cb354a5430
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2020-12-09 17:38:33 +0100

    Practica 2.1

--- media/IG2App/boyaFS.glsl
+++ media/IG2App/boyaFS.glsl
@@ -1,48 +1,52 @@
 #version 330 core
 
-in vec2 vUv0; // out del vertex shader 
+in vec2 vUv0;
 in vec3 vXxxNormal;
 in vec4 vXxxVertex;
-uniform sampler2D textura; // tipo sampler2D para texturas 2D
-uniform vec4 InColor;
-uniform vec4 OutColor;
+
+uniform sampler2D textura;
+uniform sampler2D texturaCorrosion;
+uniform vec3 InColor;
+uniform vec3 OutColor;
 uniform float Flipping;
-// uniform vec3 lightAmbient; // intensidades de la luz
-// uniform vec3 lightDiffuse;
-// uniform vec4 lightPosition;
-// uniform vec3 materialDiffuse;
+uniform vec3 lightAmbient;
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
+uniform vec3 materialDiffuse;
+
 out vec4 fFragColor; // out del fragment shader
 
+float diff(vec3 cVertex, vec3 cNormal){
+	vec3 lightDir = lightPosition.xyz;
+	if(lightPosition.w == 1){
+		lightDir = lightPosition.xyz - cVertex;
+	}
 
-// float diff(vec3 cVertex, vec3 cNormal)
-// {
-// 	vec3 lightDir = lightPosition.xyz; // directional light ?
-// 	if (lightPosition.w == 1) // positional light ?
-// 		lightDir = lightPosition.xyz ‐ cVertex;
-
-// 	return max(dot(cNormal, normalize(lightDir)), 0.0); // dot: coseno ángulo
-// }
+	return max(dot(cNormal, normalize(lightDir)), 0.0);
+}
 
 void main() {
-	vec3 colorTex = vec3(texture(textura, vUv0));
-	vec4 color;	
 
-	if(colorTex.r > 0.5) discard;
-	
-	bool frontFacing = (Flipping > -1)? gl_FrontFacing : !gl_FrontFacing; 
+	vec3 colorTexCorr = vec3(texture(texturaCorrosion, vUv0));
 
-	if(frontFacing)	color = OutColor;
-	else color = InColor;	
+	if(colorTexCorr.r > 0.5) discard;
 
-	// // ambient
-	// vec3 ambient = lightAmbient * materialDiffuse;
-	// // diffuse en view space
-	// vec3 viewVertex = vec3(modelViewMat * vertex);
-	// vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
-	// vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
-	// vFrontColor = ambient + diffuse; // + specular
-	// diffuse = diff(viewVertex, ‐viewNormal) * lightDiffuse * materialDiffuse;
-	// vBackColor = ambient + diffuse; // + specular
-	
-	fFragColor = color; // out
+	bool frontFacing = (Flipping > -1)? gl_FrontFacing : !gl_FrontFacing;
+
+	vec3 ambient = lightAmbient * materialDiffuse;
+
+	vec3 color;
+	if(frontFacing){
+		vec3 diffuse = diff(vXxxVertex.xyz, vXxxNormal) * lightDiffuse * materialDiffuse;
+		vec3 vFrontColor = ambient + diffuse;
+		vec3 colorTex = vec3(texture(textura, vUv0));
+		color = OutColor * vFrontColor * colorTex;
+	}
+	else{
+		vec3 diffuse = diff(vXxxVertex.xyz, -vXxxNormal) * lightDiffuse * materialDiffuse;
+		vec3 vBackColor = ambient + diffuse;
+		color = InColor * vBackColor;
+	}
+
+	fFragColor = vec4(color, 1.0);
 } 
