bb1715a7c7281ce2dc7fbea7ce44fe30dd257fd9
PabloVillapunMartin <pvillapu@ucm.es>
2020-12-09 12:21:56 +0100

    mitad iluminacion

--- media/IG2App/boyaFS.glsl
+++ media/IG2App/boyaFS.glsl
@@ -1,46 +1,48 @@
 #version 330 core
 
+in vec2 vUv0; // out del vertex shader 
+in vec3 vXxxNormal;
+in vec4 vXxxVertex;
 uniform sampler2D textura; // tipo sampler2D para texturas 2D
 uniform vec4 InColor;
 uniform vec4 OutColor;
 uniform float Flipping;
-in vec2 vUv0; // out del vertex shader
-uniform vec3 lightAmbient; // intensidades de la luz
-uniform vec3 lightDiffuse;
-uniform vec4 lightPosition;
-uniform vec3 materialDiffuse;
-in vec3 normal;
-in vec4 vertex; // atributos de los vértices a procesar
+// uniform vec3 lightAmbient; // intensidades de la luz
+// uniform vec3 lightDiffuse;
+// uniform vec4 lightPosition;
+// uniform vec3 materialDiffuse;
 out vec4 fFragColor; // out del fragment shader
 
-void main() {
-	// ambient
-	vec3 ambient = lightAmbient * materialDiffuse;
-	// diffuse en view space
-	vec3 viewVertex = vec3(modelViewMat * vertex);
-	vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
-	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
-	vFrontColor = ambient + diffuse; // + specular
-	diffuse = diff(viewVertex, ‐viewNormal) * lightDiffuse * materialDiffuse;
-	vBackColor = ambient + diffuse; // + specular
 
+// float diff(vec3 cVertex, vec3 cNormal)
+// {
+// 	vec3 lightDir = lightPosition.xyz; // directional light ?
+// 	if (lightPosition.w == 1) // positional light ?
+// 		lightDir = lightPosition.xyz ‐ cVertex;
+
+// 	return max(dot(cNormal, normalize(lightDir)), 0.0); // dot: coseno ángulo
+// }
+
+void main() {
 	vec3 colorTex = vec3(texture(textura, vUv0));
-	vec4 color;
+	vec4 color;	
 
 	if(colorTex.r > 0.5) discard;
 	
 	bool frontFacing = (Flipping > -1)? gl_FrontFacing : !gl_FrontFacing; 
 
-	if(frontFacing)	color = OutColor; 
+	if(frontFacing)	color = OutColor;
 	else color = InColor;	
 
-	fFragColor = diffuse; // out
+	// // ambient
+	// vec3 ambient = lightAmbient * materialDiffuse;
+	// // diffuse en view space
+	// vec3 viewVertex = vec3(modelViewMat * vertex);
+	// vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
+	// vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
+	// vFrontColor = ambient + diffuse; // + specular
+	// diffuse = diff(viewVertex, ‐viewNormal) * lightDiffuse * materialDiffuse;
+	// vBackColor = ambient + diffuse; // + specular
+	
+	fFragColor = color; // out
 } 
-
-float diff(float cVertex, float cNormal)
-{
-	vec3 lightDir = lightPosition.xyz; // directional light ?
-	if (lightPosition.w == 1) // positional light ?
-	lightDir = lightPosition.xyz ‐ cVertex;
-	return max(dot(cNormal, normalize(lightDir)), 0.0); // dot: coseno ángulo
-}
--- media/IG2App/boyaVS.glsl
+++ media/IG2App/boyaVS.glsl
@@ -1,12 +1,18 @@
 #version 330 core
 
-in vec2 uv0; // coordenadas de textura 0
-uniform mat4 modelViewProjMat; // constante de programa
-out vec3 normal;
-out vec4 vertex; // atributos de los vértices a procesar
-out vec2 vUv0; // out del vertex shader
+in vec2 uv0;
+in vec4 vertex;
+in vec3 normal;
+uniform mat4 modelViewProjMat;
+uniform mat4 modelViewMat;
+uniform mat4 normalMat;
+out vec2 vUv0; 
+out vec3 vXxxNormal;
+out vec4 vXxxVertex;
 
 void main() {
+	vXxxVertex = vec4(modelViewMat * vertex);
+	vXxxNormal = normalize(vec3(normalMat * vec4(normal,0)));
 	vUv0 = uv0; // se pasan las coordenadas de textura
 	gl_Position = modelViewProjMat * vertex; //obligatorio
 }
